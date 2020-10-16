dfd060a0d3003b46cc329acc07b277124ec1556e
Piero Lin <linyouxie@millionser.com>
2020-10-16 18:50:00 +0800

    changes

--- src/L1002_CommonChars.java
+++ src/L1002_CommonChars.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -6,7 +7,42 @@ import java.util.List;
  * https://leetcode-cn.com/problems/find-common-characters/
  */
 public class L1002_CommonChars {
+    /**
+     * 方法一：数组
+     */
     public List<String> commonChars(String[] A) {
+        // init array
+        int[] counts = new int[26];
+        Arrays.fill(counts, Integer.MAX_VALUE);
+        // loop word and count char
+        for (String word : A) {
+            int[] temp = new int[26];
+            for (char letter : word.toCharArray()) {
+                temp[letter - 'a']++;
+            }
+            // compare and update the array
+            for (int i = 0; i < 26; i++) {
+                counts[i] = Math.min(counts[i], temp[i]);
+            }
+        }
+
+        // generate result list
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < counts[i]; j++) {
+                list.add(String.valueOf((char)(i + 'a')));
+            }
+        }
+
+        return list;
+
+    }
+    /**
+     *
+     * @param A
+     * @return
+     */
+    public List<String> commonChars_2(String[] A) {
         List<String> list = new ArrayList<>();
         outer:
         for (char c : A[0].toCharArray()) {
@@ -21,4 +57,5 @@ public class L1002_CommonChars {
         }
         return list;
     }
+
 }
--- src/L24_SwapPairs.java
+++ src/L24_SwapPairs.java
@@ -6,35 +6,37 @@ public class L24_SwapPairs {
      * 解题关键：
      *  1. 交换前 2 个节点，第 3 个节点开始新一轮的递归
      */
-    public ListNode swapParis_1(ListNode head) {
-        if (head != null && head.next != null) {
-            ListNode currentNode = head;
-            ListNode nextNode = head.next;
-            ListNode nextNextNode = head.next.next;
+    public ListNode swapPairs_1(ListNode head) {
+        ListNode currentNode = head;
+        ListNode nextNode = head.next;
+        ListNode nextNextNode = head.next.next;
 
-            head = nextNode;
-            head.next = currentNode;
-            head.next.next = swapParis_1(nextNextNode);
-        }
+        head = nextNode;
+        head.next = currentNode;
+        head.next.next = swapPairs_1(nextNextNode);
 
         return head;
     }
 
     /**
      * 方法二：迭代法
+     * TC: O(n)
+     * SC: O(1)
      */
-    public ListNode swapParis_2(ListNode head) {
+    public ListNode swapPairs_2(ListNode head) {
         ListNode dummyNode = new ListNode(0);
         dummyNode.next = head;
         ListNode temp = dummyNode;
-        while (temp.next != null && temp.next.next !=null) {
+        while (temp.next != null && temp.next.next != null) {
             ListNode node1 = temp.next;
             ListNode node2 = temp.next.next;
+            ListNode node3 = temp.next.next.next;
 
             temp.next = node2;
-            node1.next = node2.next;
-            node2.next = node1;
-            temp = node1;
+            temp.next.next = node1;
+            temp.next.next.next = node3;
+
+            temp = temp.next.next;
         }
 
         return dummyNode.next;
@@ -44,15 +46,10 @@ public class L24_SwapPairs {
 class ListNode {
     int val;
     ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
+    public ListNode(int val){
         this.val = val;
     }
-
-    ListNode(int val, ListNode next) {
+    public ListNode (int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
