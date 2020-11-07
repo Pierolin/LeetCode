fa3bb672479a23e142792b8784511e81a6a06d62
piero <linyouxu@qq.com>
2020-11-07 15:44:17 +0800

    update

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,13 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Test {
 
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < 100; i++)
+        System.out.println((int)(Math.random()*10));
     }
+
+
 }
