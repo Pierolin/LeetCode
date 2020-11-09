5cd3e5853f5b40667eadf16036bfc598157e303b
Piero Lin <linyouxie@millionser.com>
2020-11-09 19:24:41 +0800

    changes

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,11 @@ import java.util.*;
 
 public class Test {
 
+
+
     public static void main(String[] args) {
-        List<List<Integer>> list = new ArrayList<>();
+        int two =   Integer.parseInt(String.valueOf('1')) * 10 + 1;
+
+        System.out.println(two);
     }
 }
