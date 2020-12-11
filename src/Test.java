5d58da57eafd67fd1f51fa7f13afc2fb0c9e648f
piero <linyouxu@qq.com>
2020-12-12 00:42:26 +0800

    update

--- src/Test.java
+++ src/Test.java
@@ -3,13 +3,7 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Test test = new Test();
-=======
-        System.out.println(16 >> 1);
-
-    }
->>>>>>> 3cbffd6cf29c9e55fca812f5a25e0f93b64c4e20
+        List<Integer>  list = new ArrayList<>();
 
     }
 
