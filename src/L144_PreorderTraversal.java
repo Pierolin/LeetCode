596097fd49461a56ab352f6a0795d973183d853b
piero <linyouxu@qq.com>
2020-12-14 01:14:52 +0800

    changes

--- src/L144_PreorderTraversal.java
+++ src/L144_PreorderTraversal.java
@@ -73,14 +73,3 @@ public class L144_PreorderTraversal {
 
 }
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    public TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Test {
-
     public static void main(String[] args) {
         List<Integer>  list = new ArrayList<>();
 
